2017af6741d5d54022994dd196dc188f47be2baa
Shantanu Mane <snake_eyes0017@yahoo.com>
2017-12-25 22:37:38 -0700

    Added constexpr WORD_LENGTH

--- BullsAndCows/BullsAndCows/main.cpp
+++ BullsAndCows/BullsAndCows/main.cpp
@@ -2,5 +2,10 @@
 
 int main()
 {
+	constexpr int WORD_LENGTH = 8;
+
+	std::cout << "Welcome to Bulls and Cows, a fun word game!\n";
+	std::cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+
 	return 0;
 }
